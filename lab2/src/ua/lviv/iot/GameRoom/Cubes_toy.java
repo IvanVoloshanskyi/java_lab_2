0be0f14f5f5577288bacc4f092c406556000619f
Ivan <ivan.voloshanskyi@gmail.com>
2022-03-31 23:32:53 +0300

    Add 2 lab code and files

---
+++ lab2/src/ua/lviv/iot/GameRoom/Cubes_toy.java
@@ -0,0 +1,8 @@
+package ua.lviv.iot.GameRoom;
+
+public class Cubes_toy  extends Toys{
+    public Cubes_toy(int price, int amount, String size) {
+        super(price, amount, size);
+    }
+
+}
---
+++ lab2/src/ua/lviv/iot/GameRoom/Doll_toy.java
@@ -0,0 +1,11 @@
+package ua.lviv.iot.GameRoom;
+
+public class Doll_toy  extends Toys{
+
+    public Doll_toy(int price, int amount, String size) {
+        super(price, amount, size);
+
+
+    }
+
+}
